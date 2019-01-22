6a2ced1e57baed3036a4b73c87e5f7a0a269b8a1
andreazampieri <andreazampieri95@gmail.com>
2019-01-22 18:46:17 +0100

    little improvements in the python key visualizer

--- show_keys.py
+++ show_keys.py
@@ -1,8 +1,12 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# returns a percentage; utility routine
+def perc(x,tot):
+	return x*100/tot
+
 MAX_INT = 2147483647
-n_bins = 800	
+n_bins = 400
 
 # parse the file that tells wether node i is sybil or not: 1 -> sybil, 0 -> not sybil
 sybil = []
@@ -23,6 +27,8 @@ layer_distribution = np.array(layer_distribution)
 
 n_nodes = len(sybil)
 n_layers = len(layer_distribution)
+n_sybil = np.sum(sybil)
+n_nonsybil = np.sum(not_sybil)
 
 #generates one subplot per layer
 colors = ["blue","red"]
@@ -33,6 +39,15 @@ subplots = subplots.flatten()
 bins = np.linspace(0,MAX_INT,n_bins) # possible values: this contains the edge-values for each bin
 for i,p in enumerate(subplots):
 	show=[layer_distribution[i][not_sybil],layer_distribution[i][sybil]]	# pair of arrays that contain respectively the non-sybil node ids and the sybil ones
-	p.hist(show,n_bins,color=colors,stacked=True)
+	p.hist(show,n_bins,color=colors,stacked=True,label=["honest","sybil"])
+	p.set_title("Layer "+str(i))
 	p.set_yscale("log")		# in order to have a nicer visualization on the clustering attack, axis y is in logscale
+
+subplots[0].legend(loc = "upper right")		# add one legend (that's valid for all the possible plots)
+plt.subplots_adjust(hspace=0.5)
+
+text_info = "Total number of nodes: {}  honest %: {:.2f}  sybil %:{:.2f}".format(n_nodes,perc(n_nonsybil,n_nodes),perc(n_sybil,n_nodes))
+mng = plt.get_current_fig_manager()
+plt.gcf().text(0.5,0.03,text_info,horizontalalignment='center',verticalalignment='bottom',fontsize=15)
+mng.window.showMaximized()
 plt.show()
